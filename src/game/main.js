43571dcc64a6b4345df2d3cde5d9586ca76aadc0
Ilan Cosman <ilancosman@gmail.com>
2016-10-01 19:36:22 -0700

    learned how to draw, changed ship to circle

--- src/game/main.js
+++ src/game/main.js
@@ -1,12 +1,23 @@
+/*
+To Do
+==========
+1. Fix Images
+2. Auto fire
+3. Auto rotate
+4. Classes
+5. Creeps
+
+*/
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.baseURL = 'http://examples.phaser.io/assets/';
+    //game.load.baseURL = 'http://examples.phaser.io/assets/';
     game.load.crossOrigin = 'anonymous';
 
     game.load.image('ship', 'sprites/thrust_ship2.png');
-    game.load.image('bullet', 'misc/bullet0.png');
+    game.load.image('bullet', './Bullet.png');
 
 }
 
@@ -19,11 +30,10 @@ var dKey;
 var sKey;
 var spaceKey;
 var cursors;
-var fireButton;
 
-var bulletTime = 0;
+var bulletTime = 0 ;
 var bullet;
-
+var tank;
 
 function create() {
 
@@ -32,7 +42,12 @@ function create() {
     bullets.setAll('checkWorldBounds', true);
     bullets.setAll('outOfBoundsKill', true);
 
-    player = game.add.sprite(400, 550, 'ship');
+    tank = game.add.graphics(0, 0);
+    tank.beginFill(0xFF0000, 1);
+    tank.drawCircle(0, 0, 33);
+
+    player = game.add.sprite(400, 550);
+    player.addChild(tank);
     game.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
 
@@ -41,7 +56,6 @@ function create() {
     sKey = game.input.keyboard.addKey(Phaser.Keyboard.S)
     dKey = game.input.keyboard.addKey(Phaser.Keyboard.D)
     cursors = game.input.keyboard.createCursorKeys();
-    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     //player.body.allowRotation = false; // the example had this but it doesn't seem to do anything
 }
@@ -66,7 +80,7 @@ function update () {
     {
         player.body.velocity.y = -600;
     }
-    if (fireButton.isDown)
+    if (game.input.activePointer.isDown)
     {
         fireBullet();
     }
@@ -83,7 +97,7 @@ function fireBullet () {
         {
             bullet.reset(player.x + 6, player.y - 12);
             game.physics.arcade.moveToPointer(bullet, 300)
-            bulletTime = game.time.time + 100;
+            bulletTime = game.time.time + 500;
         }
     }
 
