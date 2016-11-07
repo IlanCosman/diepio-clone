39ef545c8dfd2706fb6b66d840538f4e6a538244
Ilan Cosman <ilancosman@gmail.com>
2016-11-07 07:14:50 -0800

    adds creeps and collisions

--- src/game/main.js
+++ src/game/main.js
@@ -21,6 +21,7 @@ Overlords
 ===
 1. Rocket Overlord
 2. Exploder Overlord
+3. Vampire Overlord
 
 Necromancers
 ===
@@ -33,7 +34,7 @@ Traps
 ======
 1. Exploding Traps
 2. Laser Trip Wire
-3.
+3. Trap Sniper
 
 Snipers
 ======
@@ -49,6 +50,8 @@ Misc
 5. Shockwave
 6. Swordsman
 7. Spearman
+8. Zapper
+
 
 
 Tricksters
@@ -77,18 +80,13 @@ Misc
 
 */
 
-function createTriangle() {
-  //Create the triangle
-  //create triangle graphics
-  triangleGraphics = game.add.graphics(0, 0)
-  triangleGraphics.beginFill(0xFF0000, 1)
-  var scale = .4
-  triangleGraphics.drawPolygon([0, 0, 50*scale, 86.6*scale, 100*scale, 0])
-
-  triangle = creepPhysicsGroup.create(game.rnd.between(0, 500), game.rnd.between(0, 500))
-  triangle.health = triangleBodyStats.maxHealth
-  triangle.addChild(triangleGraphics)
-  game.physics.arcade.enable(triangle)
+function createCreep(stats) {
+  creep = creepPhysicsGroup.create(game.rnd.between(0, 500), game.rnd.between(0, 500))
+  creep.health = stats.bodyStats.maxHealth
+  creep.addChild(stats.bodyStats.graphicsCreator())
+  game.physics.arcade.enable(creep)
+  creep.body.drag.set(80)
+  creep.body.mass = stats.bodyStats.maxHealth * 50
 }
 
 class Stats {
@@ -121,31 +119,59 @@ class WeaponStats {
 }
 
 class BodyStats {
-  constructor(maxHealth, speed, image) {
+  constructor(maxHealth, speed, graphicsCreator) {
     this.maxHealth = maxHealth
     this.speed = speed
-    this.image = image
+    this.graphicsCreator = graphicsCreator
   }
+}
+
+
+function makeCircleGraphics() {
+  graphics = game.add.graphics(0, 0)
+  graphics.beginFill(0xFF0000, 1)
+  graphics.drawCircle(0, 0, 33)
+  return graphics
 }
 
 // Create the tank stats
 var tankWeaponStats = new WeaponStats(100, 500)
-var tankBodyStats = new BodyStats(100, 50, 'shipBody')
+var tankBodyStats = new BodyStats(100, 50, makeCircleGraphics)
 var tankStats = new Stats(tankBodyStats, [tankWeaponStats])
 
 // Create the machine gun stats
 var machineGunWeaponStats = new WeaponStats(25, 200)
-var machineGunBodyStats = new BodyStats(100, 50,'shipBody')
+var machineGunBodyStats = new BodyStats(100, 50, makeCircleGraphics)
 var machineGunStats = new Stats(machineGunBodyStats, [machineGunWeaponStats])
 
 // Creeps
-var triangleBodyStats = new BodyStats(10, 0, 'triangleBody')
+function makeTriangleGraphics() {
+  graphics = game.add.graphics(0, 0)
+  graphics.beginFill(0xFF0000, 1)
+  var scale = .4
+  graphics.drawPolygon([0, 0, 50*scale, 86.6*scale, 100*scale, 0])
+  return graphics
+}
+var triangleBodyStats = new BodyStats(10, 0, makeTriangleGraphics)
 var triangleStats = new Stats(triangleBodyStats, [])
 
-var squareBodyStats = new BodyStats(20, 0, 'squareBody')
+function makeSquareGraphics() {
+  graphics = game.add.graphics()
+  graphics.beginFill(0xFFFF00, 1)
+  graphics.drawRect(0, 0, 40, 40)
+  return graphics
+}
+var squareBodyStats = new BodyStats(20, 0, makeSquareGraphics)
 var squareStats = new Stats(squareBodyStats, [])
 
-var pentagonBodyStats = new BodyStats(40, 0, 'pentagonBody')
+function makePentagonGraphics() {
+    graphics = game.add.graphics(0, 0)
+    graphics.beginFill(0x0000FF, 1)
+    var scale = .9
+    graphics.drawPolygon([0, 0, 50*scale, 86.6*scale, 100*scale, 0])
+    return graphics
+}
+var pentagonBodyStats = new BodyStats(40, 0, makePentagonGraphics)
 var pentagonStats = new Stats(pentagonBodyStats, [])
 
 // Create the Phaser Gameawaw
@@ -162,18 +188,7 @@ var sKey
 var spaceKey
 var cursors
 
-// Graphics objects
-var bulletGraphics
-var tankGraphics
-var triangleGraphics
-var bulletBitMap
-
 var creepPhysicsGroup
-
-function collideCallback(sprite) {
-  sprite.damage(1)
-  console.log(sprite.health)
-}
 
 function preload() {
   game.load.baseURL = 'http://examples.phaser.io/assets/'
@@ -183,18 +198,15 @@ function preload() {
 
 function create() {
   creepPhysicsGroup = game.add.physicsGroup()
+  playerPhysicsGroup = game.add.physicsGroup()
 
   //Create the player
-  tankGraphics = game.add.graphics(0, 0)
-  tankGraphics.beginFill(0xFF0000, 1)
-  tankGraphics.drawCircle(0, 0, 33)
-
-  player = game.add.sprite(400, 550)
-  player.addChild(tankGraphics)
+  player = playerPhysicsGroup.create(400, 550)
+  player.addChild(makeCircleGraphics())
   game.physics.arcade.enable(player)
   player.body.collideWorldBounds = true
   player.stats = tankStats
-
+  player.health = tankStats.bodyStats.maxHealth
   // Tell the Weapon to track the 'player' Sprite, offset by 0px horizontally, 0 vertically
   player.weaponList = []
   for (weaponIndex in player.stats.weaponStatsList) {
@@ -204,10 +216,6 @@ function create() {
     weapon.trackSprite(player, 0, 0, true)
   }
 
-  createTriangle()
-  createTriangle()
-
-
   // Set up key control. Both WASD and cursors.
   aKey = game.input.keyboard.addKey(Phaser.Keyboard.A)
   wKey = game.input.keyboard.addKey(Phaser.Keyboard.W)
@@ -216,9 +224,34 @@ function create() {
   cursors = game.input.keyboard.createCursorKeys()
 }
 
-function update () {
+function collideWithDamage(sprite1, sprite2) {
+  sprite1.damage(1)
+  sprite2.damage(1)
+  // console.log(sprite1)
+  // console.log(sprite2)
+}
 
-    game.physics.arcade.collide(creepPhysicsGroup, weapon.bullets, collideCallback)
+// Checks if sprite 1 is a bullet fired by sprite2
+// function processCallback(sprite1, sprite2) {
+//   if (sprite2.weaponList == [])
+//     return true
+//
+//   return sprite1 in sprite2.weaponList[0].bullets
+// }
+
+function update () {
+    // Spawn new triangles
+    if (Math.random() <= 0.01) {
+      createCreep(triangleStats)
+    } else if (Math.random() <= 0.003) {
+      createCreep(squareStats)
+    } else if (Math.random() <= 0.001) {
+      createCreep(pentagonStats)
+    }
+
+    game.physics.arcade.collide(creepPhysicsGroup, creepPhysicsGroup)
+    game.physics.arcade.collide(weapon.bullets, creepPhysicsGroup, collideWithDamage)
+    game.physics.arcade.collide(creepPhysicsGroup, playerPhysicsGroup, collideWithDamage)
 
     player.rotation = game.physics.arcade.angleToPointer(player)
 
